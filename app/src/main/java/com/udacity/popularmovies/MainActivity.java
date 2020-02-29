150d5a18f8758cc01a6b9b75e828327cef4531c2
Ann Shumylo <annie.shumylo@gmail.com>
2020-02-29 19:11:02 +0200

    Initial commit: empty project
    Change-Id: Ie0094e8599353e26887fc6e5a298d3d23130d2f7

---
+++ app/src/main/java/com/udacity/popularmovies/MainActivity.java
@@ -0,0 +1,21 @@
+package com.udacity.popularmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.popularmovies.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
